0351848c23a046b053691dad25986e5ed21abed5
ShubhamLagad <shubhamlagad2000@gmail.com>
2022-12-04 16:21:24 +0530

    image uploading

--- app/src/main/java/com/subhdroid/hairstylers/Parlour/ParlourModel.java
+++ app/src/main/java/com/subhdroid/hairstylers/Parlour/ParlourModel.java
@@ -9,13 +9,22 @@ public class ParlourModel {
     String parlourTypes;
     String openingTime;
     String closingTime;
+    int no_of_slots;
     String address;
     String pincode;
     String photo;
 
+    public int getNo_of_slots() {
+        return no_of_slots;
+    }
+
+    public void setNo_of_slots(int no_of_slots) {
+        this.no_of_slots = no_of_slots;
+    }
+
     ParlourModel(String parlourName, String subtitle, String phone, String email, String password
-            , String parlourTypes, String openingTime, String closingTime,
-                 String address, String pincode,String photo) {
+            , String parlourTypes, String openingTime, String closingTime, int no_of_slots,
+                 String address, String pincode, String photo) {
         this.parlourName = parlourName;
         this.subtitle = subtitle;
         this.phone = phone;
@@ -24,6 +33,7 @@ public class ParlourModel {
         this.parlourTypes = parlourTypes;
         this.openingTime = openingTime;
         this.closingTime = closingTime;
+        this.no_of_slots = no_of_slots;
         this.address = address;
         this.pincode = pincode;
         this.photo = photo;
--- app/src/main/java/com/subhdroid/hairstylers/Parlour/ParlourRegistration.java
+++ app/src/main/java/com/subhdroid/hairstylers/Parlour/ParlourRegistration.java
@@ -1,29 +1,55 @@
 package com.subhdroid.hairstylers.Parlour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 import com.subhdroid.hairstylers.R;
 
 
 public class ParlourRegistration extends AppCompatActivity {
+    private final int PICK_IMAGE_REQUEST = 22;
+    private Uri filePath;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    private ImageView imageView;
+    TextView openTime, closeTime;
+    AppCompatButton btnChoose, btnUpload;
+    TextView signInTxt;
+    String imgUrl = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +59,8 @@ public class ParlourRegistration extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-        TextView openTime, closeTime;
 
-        openTime = findViewById(R.id.openTime);
-        closeTime = findViewById(R.id.closeTime);
-
-//        Parlour type
+        // Set Parlour type
         String[] parlourThreeTypes = {"Men", "Women", "Unisex"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_dropdown_item_1line, parlourThreeTypes);
@@ -46,7 +68,11 @@ public class ParlourRegistration extends AppCompatActivity {
         textView.setThreshold(3);
         textView.setAdapter(adapter);
 
-//        Opening time and closing time
+
+        // Opening time and closing time
+        openTime = findViewById(R.id.openTime);
+        closeTime = findViewById(R.id.closeTime);
+
         openTime.setOnClickListener(v -> {
 
             final Calendar c = Calendar.getInstance();
@@ -56,7 +82,12 @@ public class ParlourRegistration extends AppCompatActivity {
             TimePickerDialog timePickerDialog = new TimePickerDialog(ParlourRegistration.this,
                     (TimePickerDialog.OnTimeSetListener) (view, hourOfDay, minute1) -> {
                         String format = showTime(hourOfDay);
-                        openTime.setText(hourOfDay + ":" + minute1 + " " + format);
+                        if (minute1 == 0) {
+                            openTime.setText(hourOfDay + ":" + minute1 + "0" + " " + format);
+                        } else {
+                            openTime.setText(hourOfDay + ":" + minute1 + " " + format);
+                        }
+
                     }, hour, minute, false);
             timePickerDialog.show();
         });
@@ -70,28 +101,35 @@ public class ParlourRegistration extends AppCompatActivity {
             TimePickerDialog timePickerDialog = new TimePickerDialog(ParlourRegistration.this,
                     (TimePickerDialog.OnTimeSetListener) (view, hourOfDay, minute1) -> {
                         String format = showTime(hourOfDay);
-                        closeTime.setText(hourOfDay + ":" + minute1 + " " + format);
+                        if (minute1 == 0) {
+                            closeTime.setText(hourOfDay + ":" + minute1 + "0" + " " + format);
+                        } else {
+                            closeTime.setText(hourOfDay + ":" + minute1 + " " + format);
+                        }
                     }, hour, minute, false);
             timePickerDialog.show();
         });
 
 
-        AppCompatButton selectImageBtn, uploadImageBtn;
+        btnChoose = findViewById(R.id.btnChoose);
+        btnUpload = findViewById(R.id.btnUpload);
 
-        selectImageBtn = findViewById(R.id.selectImageBtn);
-        uploadImageBtn = findViewById(R.id.uploadImageBtn);
-
-        uploadImageBtn.setOnClickListener(new View.OnClickListener() {
+        btnChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                selectImageBtn.setText("Image.jpg");
+                SelectImage();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
             }
         });
 
 
-        TextView signInTxt;
         signInTxt = findViewById(R.id.signInTxt);
-
         signInTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -101,46 +139,16 @@ public class ParlourRegistration extends AppCompatActivity {
         });
 
 
-//        Fetching form details
-
-        EditText parlourName, subtitle, phone, email, password, cpassword, address, pincode;
-        AppCompatButton signUpBtn;
-        AutoCompleteTextView parlourType;
-
-        parlourName = findViewById(R.id.parlourName);
-        subtitle = findViewById(R.id.subtitle);
-        phone = findViewById(R.id.phone);
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        cpassword = findViewById(R.id.cpassword);
-        parlourType = findViewById(R.id.parlourType);
-        address = findViewById(R.id.address);
-        pincode = findViewById(R.id.pincode);
-
-        signUpBtn = findViewById(R.id.signUpBtn);
-
+        AppCompatButton signUpBtn = findViewById(R.id.signUpBtn);
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ParlourRegistration.this, "Sign up clicked", Toast.LENGTH_SHORT).show();
-                DatabaseReference parlourRef = FirebaseDatabase.getInstance().getReference(
-                        "parlour");
-                ParlourModel parlourModel = new ParlourModel(parlourName.getText().toString(),
-                        subtitle.getText().toString(),
-                        phone.getText().toString(), email.getText().toString(), cpassword.getText().toString(), parlourType.getText().toString(),
-                        openTime.getText().toString(), closeTime.getText().toString(), address.getText().toString(), pincode.getText().toString(), selectImageBtn.getText().toString());
-
-                String parloutID = parlourRef.push().getKey();
-
-                parlourRef.child(parloutID).setValue(parlourModel);
+                insertRecord();
             }
         });
-
-
-
-
     }
+
 
     public String showTime(int hour) {
         String format;
@@ -154,5 +162,129 @@ public class ParlourRegistration extends AppCompatActivity {
             format = "AM";
         }
         return format;
+    }
+
+
+    // Select Image method
+    private void SelectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Image from here..."), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        imageView = findViewById(R.id.imgView);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+                btnChoose.setText("Choose another image");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    // UploadImage method
+
+    private void uploadImage() {
+        if (filePath != null) {
+            ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            // Defining the child of storageReference
+            String timeStamp = new SimpleDateFormat("MMHHddmmssyyyy").format(new java.util.Date());
+            StorageReference ref = FirebaseStorage.getInstance().getReference("ParlourImages").child(timeStamp);
+
+            ref.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressDialog.dismiss();
+                    StorageReference storageReference = FirebaseStorage.getInstance().getReference("ParlourImages").child(
+                            timeStamp);
+                    Task<Uri> urlTask = storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            imgUrl = uri.toString();
+                            Toast.makeText(ParlourRegistration.this, "Image Uploaded",
+                                    Toast.LENGTH_SHORT).show();
+//                                    ImageView fimg = findViewById(R.id.fimg);
+//                                    Picasso.get().load(imageUrl).into(fimg);
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(ParlourRegistration.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                    progressDialog.setMessage("Uploaded " + (int) progress + "%");
+                }
+            });
+        }
+    }
+
+
+    private void insertRecord() {
+
+        EditText parlourName, subtitle, phone, email, cpassword, address, pincode;
+        AutoCompleteTextView parlourType;
+
+        parlourName = findViewById(R.id.parlourName);
+        subtitle = findViewById(R.id.subtitle);
+        phone = findViewById(R.id.phone);
+        email = findViewById(R.id.email);
+        cpassword = findViewById(R.id.cpassword);
+        parlourType = findViewById(R.id.parlourType);
+        address = findViewById(R.id.address);
+        pincode = findViewById(R.id.pincode);
+//        To calculate slots
+        String T1 = (openTime.getText().toString()).split(" ", 2)[0];
+        String T2 = (closeTime.getText().toString()).split(" ", 2)[0];
+        int no_of_slots = noOfSlots(T1, T2);
+
+        DatabaseReference parlourRef = FirebaseDatabase.getInstance().getReference(
+                "parlour");
+        ParlourModel parlourModel = new ParlourModel(parlourName.getText().toString(),
+                subtitle.getText().toString(),
+                phone.getText().toString(), email.getText().toString(), cpassword.getText().toString(), parlourType.getText().toString(),
+                openTime.getText().toString(), closeTime.getText().toString(),no_of_slots,
+                address.getText().toString(), pincode.getText().toString(),imgUrl);
+
+        String parloutID = parlourRef.push().getKey();
+
+        parlourRef.child(parloutID).setValue(parlourModel);
+    }
+
+
+    private int noOfSlots(String T1, String T2) {
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+
+        Date d1 = null;
+        Date d2 = null;
+        try {
+            d1 = format.parse(T1);
+            d2 = format.parse(T2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        long diff = d2.getTime() - d1.getTime();
+        long diffMinutes = diff / (60 * 1000);
+
+        return (int)diffMinutes/20;
     }
 }
